594079fec47899016fa178ca619e42c3b13a250e
nupoorb <35562572+nupoorb@users.noreply.github.com>
2023-09-14 13:34:42 -0700

    Update assignment2.ino

--- Assignment2/assignment2.ino
+++ Assignment2/assignment2.ino
@@ -2,7 +2,7 @@
 #include <LilyGoWatch.h>
 
 TTGOClass *ttgo;
-String name = "Nupoor Bibawe";
+
 
 void setup()
 {
@@ -13,11 +13,13 @@ void setup()
 
     ttgo->tft->fillScreen(TFT_WHITE);
     ttgo->tft->setTextColor(TFT_BLACK, TFT_WHITE);
+    ttgo->tft->setTextFont(4);
+
     int width = ttgo->tft->textWidth("Nupoor Bibawe");
     int height = ttgo->tft->fontHeight(4);
-    ttgo->tft->setTextFont(4);
-    int center1 = (120-width);
-    int center2 = (240-height)/2;
+
+    int center1 = (240-width) / 2;
+    int center2 = (240-height) / 2;
     ttgo->tft->drawString("Nupoor Bibawe", center1 , center2);
 }
 
